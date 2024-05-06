6cd6ff5590a82f4838697ba990cb32453be93404
Ethan Moffat <ethan@moffat.io>
2024-05-05 23:06:29 -0700

    Remove 'comment' from dummy instruction
    It is unused in the protocol and during codegen (for dummy instructions) and can be safely ignored.

--- ProtocolGenerator/Model/Xml/ProtocolDummyInstruction.cs
+++ ProtocolGenerator/Model/Xml/ProtocolDummyInstruction.cs
@@ -4,12 +4,8 @@ namespace ProtocolGenerator.Model.Xml;
 
 public sealed class ProtocolDummyInstruction
 {
-
     [XmlAttribute("type")]
     public string Type { get; set; }
-
-    [XmlElement("comment")]
-    public string Comment { get; set; }
 
     [XmlText]
     public string Content { get; set; }
