d4e3793184916128210b3777e987278402d00f3f
Ethan Moffat <ethan@moffat.io>
2024-05-14 15:21:20 -0700

    Create release.yml

--- Moffat.EndlessOnline.SDK/Properties/AssemblyInfo.cs
+++ Moffat.EndlessOnline.SDK/Properties/AssemblyInfo.cs
@@ -4,3 +4,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("Moffat.EndlessOnline.SDK")]
 [assembly: AssemblyTitle("Moffat.EndlessOnline.SDK")]
 [assembly: AssemblyCopyright("Copyright © 2024 Ethan Moffat")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
