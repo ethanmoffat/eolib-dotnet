e8667e9708a6f44e9650f7c13a91e613dd0683d2
Ethan Moffat <ethan@moffat.io>
2024-05-14 12:58:01 -0700

    Add nuget attributes/assembly info

---
+++ Moffat.EndlessOnline.SDK/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Moffat.EndlessOnline.SDK")]
+[assembly: AssemblyProduct("Moffat.EndlessOnline.SDK")]
+[assembly: AssemblyTitle("Moffat.EndlessOnline.SDK")]
+[assembly: AssemblyCopyright("Copyright © 2024 Ethan Moffat")]
